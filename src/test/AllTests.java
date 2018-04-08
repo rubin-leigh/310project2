dc2e8896b298fd4ae4b4439020ead8632a06d7d0
Leigh Rubin <leighrub@usc.edu>
2018-04-08 16:47:53 -0700

    write tests for password hashing

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ CollageHandlerTest.class, ImageTransformTest.class, MainControllerTest.class })
+@SuiteClasses({ CollageHandlerTest.class, ImageTransformTest.class, MainControllerTest.class, LoginTest.class, TestBCrypt.class})
 public class AllTests {
 
 }
