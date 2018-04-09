b316f07b158ad9fca92b8f94cb19469243d34049
Daniel Mizrahi <dmizrahi@usc.edu>
2018-04-08 17:58:12 -0700

    Added tests for image creation on backend

---
+++ src/test/TestCollageModification.java
@@ -0,0 +1,40 @@
+package test;
+
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+
+import server.*;
+import LoginFiles.*;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+
+import JUnit.JUnit;
+
+import static org.junit.Assert.*;
+
+public class TestCollageModification {
+	
+	@Test
+	public void TestLetterCreation() throws IOException {
+
+		ImageTransform it = new ImageTransform("test");
+		String testString = "test";
+		BufferedImage textImage = it.convertToTextImage(new BufferedImage, testString);
+		assertNotNull(textImage);
+		
+	}
+}
