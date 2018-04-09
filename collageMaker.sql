f77376f04a759c3a364f4b46d54d20b3b7f00927
Leigh Rubin <leighrub@usc.edu>
2018-04-08 17:23:24 -0700

    update sql script

--- collageMaker.sql
+++ collageMaker.sql
@@ -6,12 +6,14 @@ USE CollageMaker;
 CREATE TABLE Users (
 	userID INT(11) PRIMARY KEY AUTO_INCREMENT,
     userName VARCHAR(100) NOT NULL,
-    pw VARCHAR(100) NOT NULL
+    pw VARCHAR(100) NOT NULL,
+    salt VARCHAR(100) NOT NULL
 );
 
-INSERT INTO Users (userName, pw)
-	VALUES ('dan', 'password'),
-           ('bob', 'test');
+INSERT INTO Users (userName, pw, salt)
+	VALUES ('dan', '$2a$10$KoQi6QGvMRcXHbRFhqx5Le8zg5b4H08A5LTaW5dG9NVxf9VIT1Ssa', '$2a$10$KoQi6QGvMRcXHbRFhqx5Le'),
+           ('bob', '$2a$10$QGokugimymrH4lve4LyHwuf9Y9RCYMwPUM87gNh6Xt/nSafqKHRlS
+', '$2a$10$QGokugimymrH4lve4LyHwu');
 
 CREATE TABLE Collages (
 	collageID int(11) PRIMARY KEY AUTO_INCREMENT,
