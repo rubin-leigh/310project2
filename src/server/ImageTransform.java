cb063eab594bce6cc3bbc9327f05f8ca6dbac2cf
Leigh Rubin <leighrub@usc.edu>
2018-04-21 13:15:52 -0700

    image filters

--- src/server/ImageTransform.java
+++ src/server/ImageTransform.java
@@ -2,18 +2,19 @@ package server;
 
 import java.awt.AlphaComposite;
 import java.awt.BasicStroke;
-import java.awt.image.DataBufferByte;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.Shape;
+import java.awt.color.ColorSpace;
 import java.awt.font.FontRenderContext;
 import java.awt.font.GlyphVector;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorConvertOp;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -22,6 +23,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -65,11 +67,6 @@ public class ImageTransform {
 		this.filter = filter; 
 		this.letters = letters;
 		this.retrievedImages = new ArrayList<BufferedImage>();
-	}
-	
-	public BufferedImage convertToTextImage(BufferedImage image) {
-		
-		return image;
 	}
 	
 //	private Pixel[][] convertTo2D(BufferedImage image) {
@@ -249,8 +246,8 @@ public class ImageTransform {
 			double scaledWidth = (originalWidth/scaleFactor);
 			double scaledHeight = (originalHeight/scaleFactor);
 
-			Image tmp = originalImage.getScaledInstance((int)150, (int)150, Image.SCALE_SMOOTH);
-			BufferedImage resizedImage = new BufferedImage((int)150, (int)150, BufferedImage.TYPE_INT_ARGB);
+			Image tmp = originalImage.getScaledInstance((int)50, (int)50, Image.SCALE_SMOOTH);
+			BufferedImage resizedImage = new BufferedImage((int)50, (int)50, BufferedImage.TYPE_INT_ARGB);
 
 			Graphics2D g2d = resizedImage.createGraphics();
 			g2d.drawImage(tmp, 0, 0, null);
@@ -301,63 +298,65 @@ public class ImageTransform {
 		int y = 0;
 		int imageNum = 0;
 		Random rand = new Random();
-		for(BufferedImage image : this.retrievedImages) {
-//			if (imageNum == 0) {
-//				Image tmp = image.getScaledInstance((int)COLLAGE_WIDTH, (int)COLLAGE_HEIGHT, Image.SCALE_SMOOTH);
-//				BufferedImage resizedImage = new BufferedImage((int)COLLAGE_WIDTH, (int)COLLAGE_HEIGHT, BufferedImage.TYPE_INT_ARGB);
-//
-//				if(borders)
-//				{
-//					this.borderImage(resizedImage);
+		for(int i = 0; i < 30; i++) {
+			Collections.shuffle(this.retrievedImages);
+			for(BufferedImage image : this.retrievedImages) {
+//				if (imageNum == 0) {
+//					Image tmp = image.getScaledInstance((int)COLLAGE_WIDTH, (int)COLLAGE_HEIGHT, Image.SCALE_SMOOTH);
+//					BufferedImage resizedImage = new BufferedImage((int)COLLAGE_WIDTH, (int)COLLAGE_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+//	
+//					if(borders)
+//					{
+//						this.borderImage(resizedImage);
+//					}
+//	
+//					Graphics2D g2d = resizedImage.createGraphics();
+//					g2d.drawImage(tmp, 0, 0, null);
+//					g.drawImage(resizedImage, 0, 0, null);
 //				}
-//
-//				Graphics2D g2d = resizedImage.createGraphics();
-//				g2d.drawImage(tmp, 0, 0, null);
-//				g.drawImage(resizedImage, 0, 0, null);
-//			}
-//			else {
-				if(borders)
-				{
-					this.borderImage(image);
-				}
-				AffineTransform backup = g.getTransform();
-				AffineTransform imageRotator = new AffineTransform();
-				int rotationAmount = generateRotationAmount();
-				
-				if(rotations)
-				{
-					image = rotateImage(image,rotationAmount);
-					//imageRotator.rotate(Math.toRadians(rotationAmount), image.getWidth()/2, image.getHeight()/2);
-					//g.transform(imageRotator);
-				}
-
-				// randomly generates the location of the next image on the collage within the bounds of the collage
-				//x = rand.nextInt(COLLAGE_WIDTH-600)+300;
-				//y = rand.nextInt(COLLAGE_HEIGHT-500)+200;
-				
-				x = 25+(imageNum%10)*102;
-				y = 25+(imageNum/10)*175;
-				g.drawImage(image, null,x, y);
-				g.setTransform(backup);
-//			}
-
-			imageNum++;
+//				else {
+					if(borders)
+					{
+						this.borderImage(image);
+					}
+					AffineTransform backup = g.getTransform();
+					AffineTransform imageRotator = new AffineTransform();
+					int rotationAmount = generateRotationAmount();
+					
+					if(rotations)
+					{
+						image = rotateImage(image,rotationAmount);
+					}
+					
+					x = imageNum*(COLLAGE_WIDTH/30);
+					y = i*(COLLAGE_HEIGHT/30);
+					g.drawImage(image, null,x, y);
+					g.setTransform(backup);
+	//			}
+	
+				imageNum++;
+				}	
+			imageNum = 0;
+			
 		}
+		
+		this.completeImage = collageImage;
+		applyFilter();
+		
 		try {
-			ImageIO.write(collageImage,"png",new File(this.topic + "-collage.png"));
+			ImageIO.write(this.completeImage,"png",new File(this.topic + "-collage.png"));
 		} catch(IOException e) {
 			e.getMessage();
 		}
-		this.completeImage = collageImage;
-		return collageImage;
+		return this.completeImage;
 	}
 	
 	public BufferedImage generateTextImage()
 	{
 		BufferedImage originalImage = this.completeImage;
         final BufferedImage textImage = new BufferedImage(
-            originalImage.getWidth(),
-            originalImage.getHeight(),
+            COLLAGE_WIDTH,
+            COLLAGE_HEIGHT,
             BufferedImage.TYPE_INT_ARGB);
         Graphics2D g = textImage.createGraphics();
         FontRenderContext frc = g.getFontRenderContext();
@@ -451,6 +450,112 @@ public class ImageTransform {
 	// accessor method to return the complete collage image as a buffered image
 	public BufferedImage getCompleteImage() {
 		return this.completeImage;
+	}
+	
+	public void applyFilter() {
+		System.out.println(filter);
+		if (filter.equals("sepia")) {
+			applySepia();
+		} else if (filter.equals("blackAndWhite")) {
+			applyBlackAndWhite();
+		} else if (filter.equals("grayscale")) {
+			applyGrayscale();
+		}
+	}
+	
+	//https://www.dyclassroom.com/image-processing-project/how-to-convert-a-color-image-into-sepia-image
+	public void applySepia() {
+	    //get width and height of the image
+	    int width = this.completeImage.getWidth();
+	    int height = this.completeImage.getHeight();
+
+	    //convert to sepia
+	    for(int y = 0; y < height; y++){
+	      for(int x = 0; x < width; x++){
+	        int p = this.completeImage.getRGB(x,y);
+	        
+	        int a = (p>>24)&0xff;
+	        int r = (p>>16)&0xff;
+	        int g = (p>>8)&0xff;
+	        int b = p&0xff;
+
+	        //calculate tr, tg, tb
+	        int tr = (int)(0.393*r + 0.769*g + 0.189*b);
+	        int tg = (int)(0.349*r + 0.686*g + 0.168*b);
+	        int tb = (int)(0.272*r + 0.534*g + 0.131*b);
+	        
+
+	        //check condition
+	        if(tr > 255){
+	          r = 255;
+	        }else{
+	          r = tr;
+	        }
+
+	        if(tg > 255){
+	          g = 255;
+	        }else{
+	          g = tg;
+	        }
+
+	        if(tb > 255){
+	          b = 255;
+	        }else{
+	          b = tb;
+	        }
+
+	        //set new RGB value
+	        p = (a<<24) | (r<<16) | (g<<8) | b;
+	        
+	        this.completeImage.setRGB(x, y, p);
+	      }
+	    }
+	  }
+	
+	public void applyGrayscale() {
+		for (int x = 0; x < this.completeImage.getWidth(); ++x) {
+		    for (int y = 0; y < this.completeImage.getHeight(); ++y)
+		    {
+		        int p = this.completeImage.getRGB(x, y);
+		        
+		        int a = (p>>24)&0xff;
+		        int r = (p>>16)&0xff;
+		        int g = (p>>8)&0xff;
+		        int b = p&0xff;
+		        
+		        int tr = (int)(0.299*r);
+		        int tg = (int)(0.587*g);
+		        int tb = (int)(0.114*b);
+		        
+		        int tt = tr+tg+tb;
+		        
+		        int gray = (a<<24) | (tt<<16) | (tt<<8) | tt;
+		        
+		        this.completeImage.setRGB(x, y, gray);
+		    }
+		}
+	}
+	
+	public void applyBlackAndWhite() {
+		int width = this.completeImage.getWidth();
+	    int height = this.completeImage.getHeight();
+
+	    for(int y = 0; y < height; y++){
+	      for(int x = 0; x < width; x++){
+	        int p = this.completeImage.getRGB(x,y);
+	        
+	        int a = (p>>24)&0xff;
+	        int r = (p>>16)&0xff;
+	        int g = (p>>8)&0xff;
+	        int b = p&0xff;
+	        
+	        if (r+g+b > 255*3/2) {
+	        		this.completeImage.setRGB(x, y, Color.WHITE.getRGB());
+	        } else {
+	        		this.completeImage.setRGB(x, y, Color.BLACK.getRGB());
+	        }
+	      }
+	    }
 	}
 
 }
